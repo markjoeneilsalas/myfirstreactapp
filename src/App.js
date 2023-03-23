e66712b067da390a3f4fbc9db3e5aab6fad47163
markjoeneilsalas <markjoeneil.salas@gmail.com>
2023-03-24 02:48:17 +0800

    modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Bootcamp extends React.Component {
+  render() {
+    return <h1>This is my first react website</h1>;
+  }
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Bootcamp/>);
 
 export default App;
